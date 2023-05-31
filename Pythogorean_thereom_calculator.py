0ce5a485ed93fd5095a84b4720bd7eafb681f6cb
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-05-31 07:16:56 -0400

    Create Pythogorean_thereom_calculator.py

---
+++ Pythogorean_thereom_calculator.py
@@ -0,0 +1,24 @@
+import math
+def hypotenuse(legA, legB):
+  hypotenuse_squared = legA**2+legB**2
+  return(math.sqrt(hypotenuse_squared))
+def leg(legB, hypotenuse):
+  leg_squared = hypotenuse**2 - legB**2
+  return(math.sqrt(leg_squared))
+side = input("Would you like to calculate the length of the hypotenuse, or a leg? Reply With 'leg' or 'hypotenuse')
+if side.lower == leg: 
+  try:            
+    length_other_leg = float(input('What is the length of the other leg?')
+    hypotenuse_length = float(input('What is the length of the hypotenuse?')
+  except ValueError:
+    print("Please answer with a number")
+  print(leg(length_other_leg , hypotenuse_length))
+            
+                           
+if side.lower == hypotenuse:
+  try:            
+    length_other_leg = float(input('What is the length of the first leg?')
+    length_leg = float(input('What is the length of the other leg?')
+  except ValueError:
+    print("Please answer with a number")
+  print(leg(length_leg , length_leg))
