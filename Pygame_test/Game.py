ca6f02b3297a3581a2f79132f87df52e0f3a0866
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-05-31 17:25:12 -0400

    Update and rename Main.py to Game.py
    Game Basics - I hope to figure out PyGame enough that I can stop googling everything lol.

---
+++ Pygame_test/Game.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Simple Game")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+square_size = 50
+square_pos = [(WIDTH - square_size) // 2, (HEIGHT - square_size) // 2]
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        square_pos[1] -= 0.5
+    if keys[pygame.K_s]:
+        square_pos[1] += 0.5
+    if keys[pygame.K_a]:
+        square_pos[0] -= 0.5
+    if keys[pygame.K_d]:
+        square_pos[0] += 0.5
+
+    window.fill(WHITE)
+    pygame.draw.rect(window, BLUE, (square_pos[0], square_pos[1], square_size, square_size))
+    pygame.display.update()
+
+pygame.quit()
+
+
